58836e9a91df2f688b838620ab209d02b281074a
Julian Kyer <julian.kyer@gmail.com>
2017-03-20 14:03:18 -0600

    Add start of file structure

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//state and fetch live in App
+//sidebar component for random text
+//controls component with buttons for people, planets, vehicles, favorites
+//
