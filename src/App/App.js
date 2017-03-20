97129e5d72cf49fd3bf16c2632629c87bdd821b7
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-03-20 16:42:57 -0600

    added film text to sidetext

--- src/App/App.js
+++ src/App/App.js
@@ -4,18 +4,43 @@ import Controls from '../Controls/Controls';
 import SideText from '../SideText/SideText';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      film: {}
+    }
+  }
+
+
+  componentDidMount() {
+    const randomNumber = Math.floor(Math.random() * (7 - 1)) + 1;
+    fetch(`http://swapi.co/api/films/${randomNumber}/`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      this.setState({ film: json })
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header>Star Wars</header>
-        <SideText />
+        <SideText film={this.state.film}/>
         <Controls />
       </div>
     );
   }
 }
 
+App.propTypes = {
+  film: React.PropTypes.object
+};
+
 export default App;
+
+
 
 //state and fetch live in App
 //sidebar component for random text
--- src/SideText/SideText.js
+++ src/SideText/SideText.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './SideText.css'
 
-const SideText = () => {
+const SideText = ({ film }) => {
   return (
     <div className="side-text">
-      <article>heyyy</article>
+      <marquee behavior="scroll" direction="up">
+        <p className="film opening">{film.opening_crawl}</p>
+        <p className="film title">{film.title}</p>
+        <p className="film date">{film.release_date}</p>
+      </marquee>
     </div>
   )
 };
