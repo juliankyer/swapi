69148d06d65d1d85624a82f72f2d9a2d18ad1e80
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-03-26 19:29:41 -0600

    add componentDidMount test to app file

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
 
 describe('App', () => {
 
@@ -60,5 +61,14 @@ describe('App', () => {
     expect(wrapper.state().vehiclesClicked).toBe(true);
     expect(wrapper.state().planetsClicked).toBe(false);
     expect(wrapper.state().peopleClicked).toBe(false);
+  });
+
+
+  it('calls componentDidMount', () => {
+    sinon.spy(App.prototype, 'componentDidMount');
+    const wrapper = mount(<App />);
+
+    expect(App.prototype.componentDidMount).toHaveProperty('callCount', 1);
+    App.prototype.componentDidMount.restore();
   });
 });
