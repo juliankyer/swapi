ef3b8c58bb882ac5fe7919b7d26a361e0a919194
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-03-27 20:37:07 -0600

    made vehicle a class component, now favorited works for it. no testing done on it though

--- src/Card/VehicleCard.js
+++ src/Card/VehicleCard.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './VehicleCard.css';
+import classNames from 'classnames';
 
-const VehicleCard = (props) => {
 
+class VehicleCard extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      favorited: false
+    }
+  }
+
+  toggleFavorite() {
+    this.setState({ favorited: !this.state.favorited })
+  }
+
+
+  render() {
+    const identifiers = classNames({
+      'vehicle-card': this.state,
+      'card': this.state,
+      'fav-flag': this.state.favorited,
+      'unfav-flag': !this.state.favorited
+    });
     return (
-      <div className="vehicle-card">
-        <p className="vehicle-name">{ props.vehiclesInfo.name }</p>
-        <p className="vehicle-model">Model: { props.vehiclesInfo.model }</p>
-        <p className="vehicle-class">Class: { props.vehiclesInfo.vehicle_class }</p>
-        <p className="vehicle-passengers">Passengers:  { props.vehiclesInfo.passengers }</p>
-        <button className="fav" >
+      <div className={identifiers}>
+        <p className="vehicle-name">{ this.props.vehiclesInfo.name }</p>
+        <p className="vehicle-model">Model: { this.props.vehiclesInfo.model }</p>
+        <p className="vehicle-class">Class: { this.props.vehiclesInfo.vehicle_class }</p>
+        <p className="vehicle-passengers">Passengers:  { this.props.vehiclesInfo.passengers }</p>
+        <button className="fav"
+                onClick={ () => this.toggleFavorite() }>
         </button>
       </div>
     )
-  }
+  };
+};
 
+VehicleCard.propTypes = {
+  favorited: React.PropTypes.bool
+};
 
 export default VehicleCard;
